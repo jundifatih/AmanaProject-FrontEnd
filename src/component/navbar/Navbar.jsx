28f1565612f65dc4154dc61f69aefeea486492fd
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-11-14 16:58:16 +0700

    feat: Change Styling of Navbar to module css

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import "./StyleNavbar.css"
+import styles from "./StyleNavbar.module.css";
 // function Menu() {
 //   var x = document.getElementById("Links");
 //   if (x.style.display === "block") {
@@ -8,29 +8,30 @@ import "./StyleNavbar.css"
 //   }
 // }
 
-
 const Navbar = () => {
   return (
-    <div className="Navbar">
-        <div className="Logo">
-            <img src="src/assets/Logo-Amana.png" alt="Logo Amana"/>
-            <span><h1>Amana</h1></span>
-        </div>
-        <div className="Links" id="Links">
-            <a href="">Beranda</a>
-            <a href="">Berita</a>
-            <a href="">Acara</a>
-            <a href="">Tentang Kami</a>
-        </div>
-        <div className="Cari-Masuk" id="Cari-Masuk">
-            <input type="text" placeholder="Cari Masjid"/>
-            <a href="">Masuk</a>
-        </div>
-        {/* <a className="icon" onClick={Menu}>
+    <div className={styles.Navbar}>
+      <div className={styles.Logo}>
+        <img src="src/assets/Logo-Amana.png" alt="Logo Amana" />
+        <span>
+          <h1>Amana</h1>
+        </span>
+      </div>
+      <div className={styles.Links} id="Links">
+        <a href="">Beranda</a>
+        <a href="">Berita</a>
+        <a href="">Acara</a>
+        <a href="">Tentang Kami</a>
+      </div>
+      <div className={styles.CariMasuk} id="Cari-Masuk">
+        <input type="text" placeholder="Cari Masjid" />
+        <a href="">Masuk</a>
+      </div>
+      {/* <a className="icon" onClick={Menu}>
             <i className="bi bi-list"></i>
         </a> */}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
