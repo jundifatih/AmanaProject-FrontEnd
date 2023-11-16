4fe4c62f436029a9c54be0f5c68f189302c3b30b
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-11-16 17:12:03 +0700

    fix: Nav Menu mobile toggle

--- src/component/main/navbar/Navbar.jsx
+++ src/component/main/navbar/Navbar.jsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from "react";
 import styles from "./StyleNavbar.module.css";
-function Menu() {
-  // var x = document.getElementsByClassName("#Links, #Cari-Masuk");
-  var x = document.getElementById("Links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
 
 const Navbar = () => {
+  const [display, setDisplay] = useState(true);
+  const [windowSize, setWindowSize] = useState(0);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowSize > 830) {
+      setDisplay(true);
+    } else {
+      setDisplay(false);
+    }
+  }, [windowSize]);
+
   return (
     <div className={styles.Navbar}>
       <div className={styles.Logo}>
@@ -18,27 +31,46 @@ const Navbar = () => {
           <h1>Amana</h1>
         </span>
       </div>
-      <div className={styles.Links} id="Links">
-        <a href="" className={styles.List}>Beranda</a>
-        <a href="" className={styles.List}>Berita</a>
-        <a href="" className={styles.List}>Acara</a>
-        <a href="" className={styles.List}>Tentang Kami</a>
+      <div
+        className={styles.Links}
+        id="Links"
+        style={display ? { display: "block" } : { display: "none" }}
+      >
+        <a href="" className={styles.List}>
+          Beranda
+        </a>
+        <a href="" className={styles.List}>
+          Berita
+        </a>
+        <a href="" className={styles.List}>
+          Acara
+        </a>
+        <a href="" className={styles.List}>
+          Tentang Kami
+        </a>
         <div className={styles.SearchArea1}>
           <i className="bi bi-search" id={styles.IconSearch}></i>
-          <input type="text" placeholder="Cari Masjid"/>
+          <input type="text" placeholder="Cari Masjid" />
         </div>
-        <a href="" className={styles.List}>Masuk</a>
+        <a href="" className={styles.List}>
+          Masuk
+        </a>
       </div>
       <div className={styles.CariMasuk} id="Cari-Masuk">
         <div className={styles.SearchArea}>
           <i className="bi bi-search" id={styles.IconSearch}></i>
-          <input type="text" placeholder="Cari Masjid"/>
+          <input type="text" placeholder="Cari Masjid" />
         </div>
         <a href="">Masuk</a>
       </div>
-        <a className={styles.Icon} onClick={Menu}>
-            <i className="bi bi-list"></i>
-        </a>
+      <a
+        className={styles.Icon}
+        onClick={() => {
+          setDisplay(!display);
+        }}
+      >
+        <i className="bi bi-list"></i>
+      </a>
     </div>
   );
 };
