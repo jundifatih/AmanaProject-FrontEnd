0f501da4f4080d8c73e3dc2f28890fce6fa7f1f2
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-11-23 07:08:01 +0700

    feat: update router to include several new pages

--- src/lib/router/dashboardRouter.jsx
+++ src/lib/router/dashboardRouter.jsx
@@ -1,12 +1,37 @@
 import Dashboard from "../../layout/dashboard";
+import MosqueAdmins from "../../pages/dashboard/MosqueAdmins";
+import MosqueEvent from "../../pages/dashboard/MosqueEvent";
+import MosqueProfile from "../../pages/dashboard/MosqueProfile";
+import MosqueReport from "../../pages/dashboard/MosqueReport";
+import UserProfile from "../../pages/dashboard/UserProfile";
 
 const dashboardRouter = {
-  path: "/dashboard",
+  path: "/dasbor",
   element: <Dashboard />,
   children: [
     {
       index: true,
       element: <div>Dashboard</div>,
+    },
+    {
+      path: "profil-pengguna",
+      element: <UserProfile />,
+    },
+    {
+      path: "profil-masjid",
+      element: <MosqueProfile />,
+    },
+    {
+      path: "pengurus-masjid",
+      element: <MosqueAdmins />,
+    },
+    {
+      path: "acara-masjid",
+      element: <MosqueEvent />,
+    },
+    {
+      path: "laporan-masjid",
+      element: <MosqueReport />,
     },
   ],
 };
