1146814004e4f8a3e1653cd3627e9ae8adffaddd
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-11-23 07:07:28 +0700

    feat: create UserProfile Page

---
+++ src/pages/dashboard/UserProfile/UserProfile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./UserProfile.module.css";
+const UserProfile = () => {
+  return <main className={`col-10 ${styles.Main}`}>UserProfile</main>;
+};
+
+export default UserProfile;
