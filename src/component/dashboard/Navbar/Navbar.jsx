6f98fde78323c4ccb3e3feceaab340b3f2395be2
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-11-23 07:03:05 +0700

    feat: update Navbar Component

--- src/component/dashboard/Navbar/Navbar.jsx
+++ src/component/dashboard/Navbar/Navbar.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
+import Logo from "../../../assets/Logo-Amana.png";
+import Profile from "../../../assets/Profile.svg";
+import { Chevy } from "../../../assets/icons/Chevron";
+import Bars from "../../../assets/icons/Bars";
+
 const Navbar = () => {
-  return <div className={styles.Nav}>Navbar</div>;
+  const [display, setDisplay] = useState("");
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowSize >= 768) {
+      setDisplay("desktop");
+    } else if (windowSize >= 600) {
+      setDisplay("tab");
+    } else {
+      setDisplay("mobile");
+    }
+  }, [windowSize]);
+  return (
+    <nav className={styles.Nav}>
+      <div className={styles.Logo}>
+        <img src={Logo} alt="" />
+        Amana
+      </div>
+      {(display == "desktop" || display == "tablet") && (
+        <div className={styles.User}>
+          <img src={Profile} alt="" />
+          <>
+            Budiman Cahyadi
+            <Chevy />
+          </>
+        </div>
+      )}
+      {display == "mobile" && <Bars />}
+    </nav>
+  );
 };
 
 export default Navbar;
