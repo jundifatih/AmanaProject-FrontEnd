62fcbee8fde0617a27094317b7a57f4a464d52c3
jundifatih <muha21150ti@student.nurulfikri.ac.id>
2023-11-28 14:55:31 +0700

    Update Halaman Acara Responsive

--- src/pages/main/acara/Acara.jsx
+++ src/pages/main/acara/Acara.jsx
@@ -17,13 +17,39 @@ const data = DataAcaraMasjid.map((item)=>{
     })
   return (
     <div className={styles.Acara}>
-        <div className={styles.Judul}>
-            <h2>Acara Mendatang</h2>
-            <button>Lainnya</button>
+      <div className={styles.JumbrotonAcara}>
+        <div className={styles.JumbrotonKiri}>
+          <img src="src/assets/ImageAcara/IconAcara.png" alt="Icon" />
         </div>
-        <div className={styles.KomponenAcara}>
-            {data}
+        <div className={styles.JumbotonKanan}>
+          <h1>Jelajahi agenda acara terkini.</h1>
+          <p>Temukan transformasi diri melalui acara islami untuk memperkaya pemahaman agama, dan menginspirasi tindakan
+          positif dalam setiap langkah hidup.</p>
         </div>
+      </div>
+      <div className={styles.SearchAcara}>
+        <div className={styles.ContainerSearch}>
+          <div className={styles.Search}>
+            <p>Cari Acara</p> <br />
+            <input type="text" placeholder="Nama Acara"/>
+          </div>
+          <div className={styles.Search}>
+            <p>Cari Lokasi</p> <br />
+            <input type="text" placeholder="Nama Lokasi"/>
+          </div>
+          <div className={styles.Search}>
+            <p>Pilih Tanggal</p> <br />
+            <input type="date"/>
+          </div>
+        </div>
+      </div>
+      <div className={styles.Judul}>
+          <h2>Acara Mendatang</h2>
+          <button>Lainnya</button>
+      </div>
+      <div className={styles.KomponenAcara}>
+          {data}
+      </div>
     </div>
   )
 }
