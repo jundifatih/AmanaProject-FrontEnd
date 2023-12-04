298b27314a6611971f79c0b54a8ce702a1deaf16
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-12-05 06:02:46 +0700

    feat: create mosque report page

--- src/pages/dashboard/MosqueReport/MosqueReport.jsx
+++ src/pages/dashboard/MosqueReport/MosqueReport.jsx
@@ -1,7 +1,149 @@
 import React from "react";
+import styles from "./MosqueReport.module.css";
+import { Link } from "react-router-dom";
 
 const MosqueReport = () => {
-  return <div>MosqueReport</div>;
+  return (
+    <main className={`col-10 ${styles.Main}`}>
+      <Link to="/dasbor" className={`${styles.Breadcrumb}`}>
+        Laporan Masjid
+      </Link>
+      <div className={`${styles.Row}`}>
+        <div className={`col-12 ${styles.Container}`}>
+          <div className={`${styles.Row} ${styles.Header}`}>
+            Laporan Bulanan Masjid Al Ikhlas
+          </div>
+          <div className={`${styles.Row} ${styles.Body}`}>
+            <div className={`${styles.Card}`}>
+              <h2>Rp 5.000.000</h2>
+              Pemasukan
+            </div>
+            <div className={`${styles.Card}`}>
+              <h2>Rp 6.000.000</h2>Pengeluaran
+            </div>
+            <div className={`${styles.Card}`}>
+              <h2>Rp 15.000.000</h2>Kas Saat ini
+            </div>
+            <h2>Laporan Keunganan Masjid Al Ikhlas</h2>
+            <table className="col-12">
+              <thead>
+                <tr>
+                  <td>No</td>
+                  <td>Hari/Tanggal</td>
+                  <td>Uraian</td>
+                  <td>Debet</td>
+                  <td>Kredit</td>
+                  <td>Saldo</td>
+                  <td>Keterangan</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default MosqueReport;
