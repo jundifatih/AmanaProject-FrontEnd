71cd67c8254a861c87f0482109ed471cd13dc2e0
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-12-05 04:48:02 +0700

    feat: create mosque admins page

--- src/pages/dashboard/MosqueAdmins/MosqueAdmins.jsx
+++ src/pages/dashboard/MosqueAdmins/MosqueAdmins.jsx
@@ -1,7 +1,66 @@
 import React from "react";
-
+import styles from "./MosqueAdmins.module.css";
+import Image from "../../../assets/Profile.svg";
+import Image2 from "/src/assets/ImageTim/Jundi.png";
+import { Link } from "react-router-dom";
 const MosqueAdmins = () => {
-  return <div>MosqueAdmins</div>;
+  return (
+    <main className={`col-10 ${styles.Main}`}>
+      <Link to="/dasbor" className={`${styles.Breadcrumb}`}>
+        Pengurus Masjid
+      </Link>
+      <div className={`${styles.Row}`}>
+        <div className={`col-12 ${styles.Container}`}>
+          <div className={`${styles.Row} ${styles.Header}`}>
+            Daftar Pengurus Masjid
+            <button>tambah pengurus</button>
+          </div>
+          <div className={`${styles.Row} ${styles.Body}`}>
+            <div className={`col-5 ${styles.Card}`}>
+              <div className={`col-4 ${styles.CardBanner}`}>
+                <img
+                  src={Image}
+                  className={`${styles.Img}`}
+                  alt="Banner acara"
+                />
+              </div>
+              <div className={`col-8 ${styles.CardBody}`}>
+                <div className={` ${styles.CardItem}`}>
+                  <h3>Budiman Cahyadi</h3>
+                  <small>administrator web | 085777227772</small>
+                </div>
+                <div className={`${styles.Row} ${styles.Buttons}`}>
+                  <button className={`${styles.Success}`}>Lihat</button>
+                  <button className={`${styles.Primary}`}>Edit</button>
+                  <button className={`${styles.Danger}`}>Hapus</button>
+                </div>
+              </div>
+            </div>
+            <div className={`col-5 ${styles.Card}`}>
+              <div className={`col-4 ${styles.CardBanner}`}>
+                <img
+                  src={Image2}
+                  className={`${styles.Img}`}
+                  alt="Banner acara"
+                />
+              </div>
+              <div className={`col-8 ${styles.CardBody}`}>
+                <div className={`${styles.CardItem}`}>
+                  <h3>Muhammad Jundi Fatih</h3>
+                  <small>bendahara | 0812345678</small>
+                </div>
+                <div className={`${styles.Row} ${styles.Buttons}`}>
+                  <button className={`${styles.Success}`}>Lihat</button>
+                  <button className={`${styles.Primary}`}>Edit</button>
+                  <button className={`${styles.Danger}`}>Hapus</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default MosqueAdmins;
