c43faa044b4e2ceb308f7adc644a77092111e9aa
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-12-12 06:36:15 +0700

    feat: add check auth handling

--- src/layout/dashboard/Dashboard.jsx
+++ src/layout/dashboard/Dashboard.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 import Navbar from "../../component/dashboard/Navbar";
 import Sidebar from "../../component/dashboard/Sidebar";
+import { useAuth } from "../../lib/middleware/useAuth";
 import "./style.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const { isAuthenticated } = await useAuth();
+
+      if (!isAuthenticated) {
+        navigate("/login");
+      }
+    };
+
+    checkAuth();
+  }, [navigate]);
+
   return (
     <>
       <Navbar />
