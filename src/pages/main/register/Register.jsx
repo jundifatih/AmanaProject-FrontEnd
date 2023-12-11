f31c99d76735d8777fa9f9e3a3d9eaeb4408e362
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-12-12 06:36:31 +0700

    feat: add check auth

--- src/pages/main/register/Register.jsx
+++ src/pages/main/register/Register.jsx
@@ -1,38 +1,61 @@
-import { Link } from "react-router-dom";
-import styles from "./Register.module.css"
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./Register.module.css";
+import { useEffect } from "react";
+import { useAuth } from "../../../lib/middleware/useAuth";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const { isAuthenticated } = await useAuth();
+
+      if (isAuthenticated) {
+        navigate("/dasbor");
+      }
+    };
+
+    checkAuth();
+  }, [navigate]);
   return (
     <div className={styles.Container}>
-        <div className={styles.Kiri}>
-            <h1>Amana</h1>
-            <h2>Ini adalah portal untuk masuk dan mendaftar.</h2>
-            <p>Mari bergabung dengan website kami!</p>
-        </div>
-        <div className={styles.Kanan}>
-          <h1>Buat Akun</h1>
-          <div className={styles.KananForm}>
-            <label htmlFor="email">Nama Lengkap</label>
-            <input type="email" placeholder="Masukan nama lengkap" required/>
-            <label htmlFor="email">Email</label>
-            <input type="email" placeholder="Masukan email" required/>
-            <label htmlFor="password">Password</label>
-            <input type="password" placeholder="Masukan password" required />
-            <p>Atau gunakan akun</p>
-            <div className={styles.KananIcon}>
-              <i className="bi bi-facebook"></i>
-              <i className="bi bi-google"></i>
-              <i className="bi bi-twitter-x"></i>
-            </div>
-            <Link to="/login">
-              <button type="submit">Masuk</button>
-            </Link>
-            <p>Dengan masuk kamu menyetujui <span>Syarat & Ketentuan</span> dan <span>Kebijakan Privasi</span> Amana</p>
-            <p>Sudah punya akun? <span><Link to="/login">Masuk</Link></span></p>
+      <div className={styles.Kiri}>
+        <h1>Amana</h1>
+        <h2>Ini adalah portal untuk masuk dan mendaftar.</h2>
+        <p>Mari bergabung dengan website kami!</p>
+      </div>
+      <div className={styles.Kanan}>
+        <h1>Buat Akun</h1>
+        <div className={styles.KananForm}>
+          <label htmlFor="email">Nama Lengkap</label>
+          <input type="email" placeholder="Masukan nama lengkap" required />
+          <label htmlFor="email">Email</label>
+          <input type="email" placeholder="Masukan email" required />
+          <label htmlFor="password">Password</label>
+          <input type="password" placeholder="Masukan password" required />
+          <p>Atau gunakan akun</p>
+          <div className={styles.KananIcon}>
+            <i className="bi bi-facebook"></i>
+            <i className="bi bi-google"></i>
+            <i className="bi bi-twitter-x"></i>
           </div>
+          <Link to="/login">
+            <button type="submit">Masuk</button>
+          </Link>
+          <p>
+            Dengan masuk kamu menyetujui <span>Syarat & Ketentuan</span> dan{" "}
+            <span>Kebijakan Privasi</span> Amana
+          </p>
+          <p>
+            Sudah punya akun?{" "}
+            <span>
+              <Link to="/login">Masuk</Link>
+            </span>
+          </p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Register;
